ddd24864e2b6f5b3908a8f6c4808a397840d80d4
Meena.Hoda <meena.hoda@wmfs.net>
2020-01-27 16:24:48 +0000

    style: standard --fix

--- test/test.js
+++ test/test.js
@@ -8,7 +8,7 @@ const pgConcat = require('./../lib')
 
 describe('Run some basic tests', function () {
   it('Should generate an SQL statement', function (done) {
-    let statement = pgConcat(
+    const statement = pgConcat(
       [
         { columnName: 'incident_no' },
         '/',
@@ -21,13 +21,13 @@ describe('Run some basic tests', function () {
   })
 
   it('Should return undefined when 0 parts passed', function (done) {
-    let statement = pgConcat()
+    const statement = pgConcat()
     expect(statement).to.equal(undefined)
     done()
   })
 
   it('Should return undefined when 1 part passed and it isn\'t a table name', function (done) {
-    let statement = pgConcat([ { columnName: 'incident_number' } ])
+    const statement = pgConcat([{ columnName: 'incident_number' }])
     expect(statement).to.not.equal(null)
     expect(statement).to.be.a('string')
     done()
